27056ffc8dac2af9690f46d85a29b7297d5456d3
radhanama <radhanama@hotmail.com>
2022-10-23 10:22:54 -0300

    charge de data

---
+++ parquet.R
@@ -0,0 +1,9 @@
+library('xml2')
+library('arrow')
+
+file_url <- "https://mapmob.eic.cefet-rj.br/data/busdata/G1-2022-01-01.parquet"
+tf <- "data.parquet"
+download.file(file_url, destfile =  tf)
+df <- read_parquet(tf)
+head(df)
+
