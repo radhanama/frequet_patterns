38b571b9620f5d140b66ea072f665a55dfb09099
radhanama <radhanama@hotmail.com>
2022-11-16 20:40:13 -0300

    inclusao apriori

--- parquet.R
+++ parquet.R
@@ -1,10 +1,20 @@
 library('xml2')
+if(!require('arrow')) {
+  install.packages('arrow')
+  library('arrow')
+}
 library('arrow')
+if(!require('arules')) {
+  install.packages('arules')
+  library('arules')
+}
 
-file_url <- "https://mapmob.eic.cefet-rj.br/data/busdata/G1-2022-01-01.parquet"
+file_url <- "https://mapmob.eic.cefet-rj.br/data/busdata/database/G1-2022-01-01.parquet"
 tf <- "data.parquet"
 download.file(file_url, destfile =  tf)
 df <- read_parquet(tf)
 head(df)
+df
 
-realengo <- df[LINE == "383.0"]
+rules <- apriori(df,parameter = list(supp = 0.5, conf = 0.9, target = "rules"))
+summary(rules)
